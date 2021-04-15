5268628870ba4197efcc5cce6b3b7a9a51156140
atrox3d <atrox3d@gmail.com>
2021-04-15 14:03:46 +0200

    experiment reflection

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 # https://www.tutorialspoint.com/sqlalchemy/sqlalchemy_core_connecting_to_database.htm
 import sqlalchemy
 import os
+import json
+import pprint
 
 DB_FILENAME = 'college.db'
 
+# start from scratch
 try:
     os.remove(DB_FILENAME)
 except FileNotFoundError:
     pass
 
-engine = sqlalchemy.create_engine(f'sqlite:///{DB_FILENAME}', echo=True)
+# create engine
+engine = sqlalchemy.create_engine(
+    f'sqlite:///{DB_FILENAME}',     # db file
+    echo=True                       # log sql statements
+)
+# print engine property
 print(engine.driver)
 
 # deprecated
@@ -17,17 +25,32 @@ print(engine.driver)
 # deprecated
 # sqlalchemy.engine.reflection.Inspector.from_engine(engine).get_table_names()
 
+# create inspector to list tables
 inspector: sqlalchemy.engine.reflection.Inspector = sqlalchemy.inspect(engine)
 print(inspector.get_table_names())
 
+# metadata object: contains all the definitions
 meta = sqlalchemy.MetaData()
 
+# create table and update metadata
 students = sqlalchemy.Table(
-    'students',
-    meta,
-    sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
+    'students',                 # table name
+    meta,                       # metadata object
+    sqlalchemy.Column(          # define table column
+        'id',                   # column name
+        sqlalchemy.Integer,     # column type
+        primary_key=True        # primary key
+    ),
     sqlalchemy.Column('name', sqlalchemy.String),
     sqlalchemy.Column('lastname', sqlalchemy.String)
 )
 
+# create all tables
 meta.create_all(engine)
+
+# try reflection
+tablenames = inspector.get_table_names()
+print(type(tablenames))
+print(tablenames)   # empty list
+tablenames = meta.tables
+pprint.pprint(tablenames.items(), indent=4)
