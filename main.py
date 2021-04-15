7b854475bb79eb5219ca46fc779e42f2a123c308
atrox3d <atrox3d@gmail.com>
2021-04-15 11:57:33 +0200

    cleanup imports

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # https://www.tutorialspoint.com/sqlalchemy/sqlalchemy_core_connecting_to_database.htm
 import sqlalchemy
-from sqlalchemy import create_engine
 import os
 
 DB_FILENAME = 'college.db'
@@ -10,11 +9,13 @@ try:
 except FileNotFoundError:
     pass
 
-engine = create_engine(f'sqlite:///{DB_FILENAME}', echo=True)
+engine = sqlalchemy.create_engine(f'sqlite:///{DB_FILENAME}', echo=True)
 print(engine.driver)
+
 # deprecated
-# print(engine.table_names())
+# print(sqlalchemy.engine.table_names())
 # deprecated
-# Inspector.from_engine(engine).get_table_names()
+# sqlalchemy.engine.reflection.Inspector.from_engine(engine).get_table_names()
+
 inspector: sqlalchemy.engine.reflection.Inspector = sqlalchemy.inspect(engine)
 print(inspector.get_table_names())
